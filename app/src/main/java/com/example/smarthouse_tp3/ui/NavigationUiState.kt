154bee1d408cd997e13d6e175615f5ec21e4535f
thomi <tpark@itba.edu.ar>
2023-06-18 03:50:34 -0300

    Favourite navigation done

--- app/src/main/java/com/example/smarthouse_tp3/ui/NavigationUiState.kt
+++ app/src/main/java/com/example/smarthouse_tp3/ui/NavigationUiState.kt
@@ -1,7 +1,6 @@
 package com.example.smarthouse_tp3.ui
 
 import com.example.smarthouse_tp3.Device
-import com.example.smarthouse_tp3.DeviceLight
 import com.example.smarthouse_tp3.Routine
 
 data class NavigationUiState(
